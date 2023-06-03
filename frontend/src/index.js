358241954339044c200cea702bb4f14044aec765
velibegov <velibegov@mail.ru>
2023-06-03 18:29:53 +0300

    add base structure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import init from './init.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const app = async () => {
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(await init());
+};
+
+app();
